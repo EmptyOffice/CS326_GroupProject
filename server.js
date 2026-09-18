0247274e4ee5561422b16397c1dd9f0791e9367d
willibilli323 <hbae@umass.edu>
2026-09-18 15:07:05 -0400

    Add homepage

---
+++ server.js
@@ -0,0 +1,16 @@
+import express from 'express';
+
+const app = express();
+const port = 3000;
+
+app.get('/', (req, res) => {
+  res.send(`
+    <h1>Welcome to the Pioneer Valley Tennis Matchmaker</h1>
+    <p>Find local hitting partners and track court availability in the Pioneer Valley Area.</p>
+    <p>Head over to <code>/players</code> to find a match.</p>
+  `);
+});
+
+app.listen(port, () => {
+  console.log(`Matchmaker server is running at http://localhost:${port}`);
+});
