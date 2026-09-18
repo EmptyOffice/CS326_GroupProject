385455f12b20ae91260c7d82ff1551e723a3ef18
willibilli323 <hbae@umass.edu>
2026-09-18 15:16:01 -0400

    Resolve merge conflict between homepage and listings route

--- server.js
+++ server.js
@@ -1,16 +1,26 @@
 import express from 'express';
 
 const app = express();
-const port = 3000;
+const PORT = process.env.PORT || 3000;
+
+app.use(express.static('public'));
 
 app.get('/', (req, res) => {
   res.send(`
     <h1>Welcome to the Pioneer Valley Tennis Matchmaker</h1>
     <p>Find local hitting partners and track court availability in the Pioneer Valley Area.</p>
-    <p>Head over to <code>/players</code> to find a match.</p>
+    <p>Head over to <code>/Listings</code> to find a match.</p>
   `);
 });
 
-app.listen(port, () => {
-  console.log(`Matchmaker server is running at http://localhost:${port}`);
+app.get("/Listings", (req, res) => {
+  res.send("This is where tennis matches near you will likely go!")
+});
+
+app.use((req, res) => {
+  res.status(404).send('Page not found.');
+});
+
+app.listen(PORT, () => {
+  console.log(`Matchmaker server is running at http://localhost:${PORT}`);
 });
