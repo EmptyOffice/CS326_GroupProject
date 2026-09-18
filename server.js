39a993b974bc57c026c2456890c6e566e412aa2d
Edelson Garcia <egarcia@umass.edu>
2026-09-18 14:57:44 -0400

    create listings route

--- server.js
+++ server.js
@@ -5,6 +5,10 @@ const PORT = process.env.PORT || 3000;
 
 app.use(express.static('public'));
 
+app.get("/Listings", (req, res) => {
+  res.send("This is where tennis matches near you will likely go!")
+});
+
 app.use((req, res) => {
   res.status(404).send('Page not found.');
 });
